3e8665788378151bfb5bd58d0705a93f8b353a17
anuroopharshini <pandiri.harshini@gmail.com>
2022-06-26 20:05:46 -0700

    resetchnages

--- GithubMavenExample/src/test/java/WebDriverTest/WebDriverTest/AppTest2.java
+++ GithubMavenExample/src/test/java/WebDriverTest/WebDriverTest/AppTest2.java
@@ -5,10 +5,16 @@ public class AppTest2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+
 		String s="hello";
 		String s1="null";
 		s1=s1.concat(s);
 		System.out.println(s1);
+		System.out.println(s1.length());
+		
+	
+	
+
 
 	}
 
