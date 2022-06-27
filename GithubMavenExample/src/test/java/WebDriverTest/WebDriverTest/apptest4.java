1481b61e8e5287538cd370615d74586b118c92f2
anuroopharshini <pandiri.harshini@gmail.com>
2022-06-26 20:17:41 -0700

    apptest4onecommit

---
+++ GithubMavenExample/src/test/java/WebDriverTest/WebDriverTest/apptest4.java
@@ -0,0 +1,12 @@
+package WebDriverTest.WebDriverTest;
+
+public class apptest4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println( "Hello World!" );
+	    System.out.println( "Hello World! featur
+	}
+
+}
