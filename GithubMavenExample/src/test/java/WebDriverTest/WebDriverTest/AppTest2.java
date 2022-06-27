3558673277986b1620754953e95c940b0fc9af96
anuroopharshini <pandiri.harshini@gmail.com>
2022-06-26 19:18:10 -0700

    apptest2

---
+++ GithubMavenExample/src/test/java/WebDriverTest/WebDriverTest/AppTest2.java
@@ -0,0 +1,14 @@
+package WebDriverTest.WebDriverTest;
+
+public class AppTest2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="hello";
+		String s1="null";
+		s1=s1.concat(s);
+		System.out.println(s1);
+
+	}
+
+}
